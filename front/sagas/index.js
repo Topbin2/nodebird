742fb60c27e899354a1c6f8c6ecc3485667fe22c
MoSangBin <mosangbin94@gmail.com>
2022-06-28 22:17:31 +0900

    axios 설치, rootSaga 추가

--- front/sagas/index.js
+++ front/sagas/index.js
@@ -1,3 +1,30 @@
+import { all, fork, take, call, put } from "redux-saga/effects";
+import axios from "axios";
+
+function logInAPI() {
+  return axios.post("/api/login");
+}
+
+function* logIn() {
+  const result = yield call(logInAPI);
+  yield put({
+    type: "LOG_IN_SUCCESS",
+    data: result.data,
+  });
+}
+
+function* watchLogin() {
+  yield take("LOG_IN", logIn);
+}
+
+function* watchLogout() {
+  yield take("LOG_OUT");
+}
+
+function* watchAddPost() {
+  yield take("ADD_POST");
+}
+
 export default function* rootSaga() {
-  
+  yield all([fork(watchLogin), fork(watchLogout), fork(watchAddPost)]);
 }
