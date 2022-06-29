7af246ec6b9f265e7efc151ee2e3dc24004a2237
MoSangBin <mosangbin94@gmail.com>
2022-06-29 23:18:58 +0900

    post saga: addComment 추가

--- front/sagas/post.js
+++ front/sagas/post.js
@@ -5,6 +5,9 @@ import {
   ADD_POST_REQUEST,
   ADD_POST_SUCCESS,
   ADD_POST_FAILURE,
+  ADD_COMMENT_REQUEST,
+  ADD_COMMENT_SUCCESS,
+  ADD_COMMENT_FAILURE
 } from "../reducers/post";
 
 // function addPostAPI(data) {
@@ -27,10 +30,34 @@ function* addPost() {
   }
 }
 
+// function addCommentAPI(data) {
+//   return axios.post(`/api/post/${data.postId}/comment`, data);
+// }
+
+function* addComment() {
+  //! parameter -> action
+  try {
+    // const result = yield call(addCommentAPI, action.data);
+    yield delay(1000);
+    yield put({
+      type: ADD_COMMENT_SUCCESS,
+    });
+  } catch (err) {
+    yield put({
+      type: ADD_COMMENT_FAILURE,
+      data: err.response.data,
+    });
+  }
+}
+
 function* watchAddPost() {
   yield takeLatest(ADD_POST_REQUEST, addPost);
 }
 
+function* watchAddComment() {
+  yield takeLatest(ADD_COMMENT_REQUEST, addComment)
+}
+
 export default function* postSaga() {
-  yield all([fork(watchAddPost)]);
+  yield all([fork(watchAddPost), fork(watchAddComment)]);
 }
